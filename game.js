c0b5dfc212afb8eaf72a96238ad66eec5fea6f3d
Justin Mitchell <15881261+mitchjus01@users.noreply.github.com>
2023-01-19 17:33:38 -0500

    Create playRound function

--- game.js
+++ game.js
@@ -10,3 +10,27 @@ function getComputerChoice() {
   }
 }
 
+function playRound(playerSelection, computerSelection) {
+  let a = playerSelection.toUpperCase();
+  let b = computerSelection.toUpperCase();
+
+  if (a === b) {
+    return "It's a tie!";
+  } else if (a == 'ROCK' && b == 'PAPER') {
+    return "You Lose! Paper beats Rock";
+  } else if (a == 'ROCK' && b == 'SCISSORS') {
+    return "You Win! Rock beats Scissors";
+  } else if (a == 'PAPER' && b == 'ROCK') {
+    return "You Win! Paper beats Rock";
+  } else if (a == 'PAPER' && b == 'SCISSORS') {
+    return "You Lose! Scissors beats Paper";
+  } else if (a == 'SCISSORS' && b == 'ROCK') {
+    return "You Lose! Rock beats Scissors";
+  } else {
+    return "You Win! Scissors beats Paper";
+  }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
